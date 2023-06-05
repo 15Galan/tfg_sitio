e5f307a4e4dfd8a28dea2b2d59991f76ac537bbc
Sr. Galán <antoniogalanG15@gmail.com>
2023-06-05 18:13:32 +0200

    refactor: script con mejores comentarios y código más legible

--- scripts/stop-1h-container.sh
+++ scripts/stop-1h-container.sh
@@ -1,50 +1,43 @@
 #!/bin/bash
 
 
-# Contenedores activos (IDs)
-CONTENEDORES=$(docker ps -q)
-
-# Tiempo de vida de un contenedor (en segundos)
-VIDA=3600
+# Variables "globales"
+CONTENEDORES=$(docker ps -q)    # Lista de contenedores activos (IDs)
+VIDA=3600                       # Tiempo de vida de un contenedor (en segundos)
 
 
-# Mostrar la hora de ejecución del script (negrita)
-echo "\e[1mEjecución actual: $(date +%H:%M:%S)\e[0m"
-echo ""
-
-# Mostrar la cantidad de contenedores activos
-echo "Hay $(echo $CONTENEDORES | wc -w) contenedores activos."
-echo ""
+# Mostrar información de la ejecución
+echo -e "\e[1mEjecución actual: $(date +%H:%M:%S)\e[0m\n"
+if [ -z "$CONTENEDORES" ]; then
+    echo -e "No hay contenedores activos.\n"
+else
+    echo -e "Hay $(echo $CONTENEDORES | wc -w) contenedores activos.\n"
+fi
 
 
 # Recorrer los contenedores activos
 for CONTENEDOR in $CONTENEDORES; do
-    # Tiempo de ejecución del contenedor (ISO 8601)
-    TIEMPO=$(docker inspect --format '{{ .State.StartedAt }}' $CONTENEDOR)
+    # Tiempo de creación del contenedor (ISO 8601)
+    CREACION=$(docker inspect --format '{{ .State.StartedAt }}' $CONTENEDOR)
 
-    # Tiempo actual (en segundos)
-    AHORA=$(date +%s)
-    
-    # Tiempo de ejecución (en segundos)
-    TIEMPO=$(date --date="$TIEMPO" +%s)
-    
-    # Diferencia de tiempo (en segundos)
-    DIFF=$(( $AHORA - $TIEMPO ))
+    INICIO=$(date --date="$CREACION" +%s)       # Tiempo de ejecución (en segundos)
+    AHORA=$(date +%s)                           # Tiempo actual (en segundos)
+    DIFF=$(( $AHORA - $INICIO ))                # Tiempo transcurrido (en segundos)
 
     # Crear información del estado de un contenedor
     MSG="$CONTENEDOR:\t$DIFF / $VIDA segundos."
 
-
     # Comprobar que el contenedor superó el tiempo de vida
     if [ $DIFF -gt $VIDA ]; then
-        echo -e "$MSG \e[31mTiempo alcanzado.\e[0m"
+        echo -e "$MSG \e[31mTiempo de vida alcanzado.\e[0m"
         
         docker stop $CONTENEDOR > /dev/null
+
     else
         if [ $DIFF -gt $(( $VIDA - 60 )) ]; then
-            echo -e "$MSG \e[33mTiempo escaso.\e[0m"
+            echo -e "$MSG \e[33mDestrucción inminente.\e[0m"
         else
-            echo -e "$MSG \e[32mTiempo disponible.\e[0m"
+            echo -e "$MSG \e[32mDisponible.\e[0m"
         fi
     fi
 done
